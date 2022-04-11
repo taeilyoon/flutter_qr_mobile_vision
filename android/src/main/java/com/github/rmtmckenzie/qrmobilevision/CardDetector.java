dc38986705bc5d3bfac7320c80a969be4c703837
taeilyoon <zkrxpdlf0808@gmail.com>
2022-04-11 11:53:16 +0900

    sss

---
+++ android/src/main/java/com/github/rmtmckenzie/qrmobilevision/CardDetector.java
@@ -0,0 +1,91 @@
+package com.github.rmtmckenzie.qrmobilevision;
+
+import android.util.Log;
+
+import androidx.annotation.GuardedBy;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
+
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import java.util.List;
+
+/**
+ * Allows QrCamera classes to send frames to a Detector
+ */
+
+class CardDetector implements OnSuccessListener<Map<string, string>>, OnFailureListener {
+    private static final String TAG = "cgr.qrmv.QrDetector";
+    private final QrReaderCallbacks communicator;
+    private final TextRecognizer detector;
+
+    public interface Frame {
+        InputImage toImage();
+
+        void close();
+    }
+
+    @GuardedBy("this")
+    private Frame latestFrame;
+
+    @GuardedBy("this")
+    private Frame processingFrame;
+
+    CardDetector(QrReaderCallbacks communicator, BarcodeScannerOptions options) {
+        this.communicator = communicator;
+        this.detector =TextRecognition.getClient();
+    }
+
+    void detect(Frame frame) {
+        if (latestFrame != null) latestFrame.close();
+        latestFrame = frame;
+
+        if (processingFrame == null) {
+            processLatest();
+        }
+    }
+
+    private synchronized void processLatest() {
+        if (processingFrame != null) processingFrame.close();
+        processingFrame = latestFrame;
+        latestFrame = null;
+        if (processingFrame != null) {
+            processFrame(processingFrame);
+        }
+    }
+
+    private void processFrame(Frame frame) {
+        InputImage image;
+        try {
+            image = frame.toImage();
+        } catch (IllegalStateException ex) {
+            // ignore state exception from making frame to image
+            // as the image may be closed already.
+            return;
+        }
+
+        detector.process(image)
+            .addOnSuccessListener(this)
+            .addOnFailureListener(this);
+    }
+
+    @Override
+    public void onSuccess(List<Barcode> firebaseVisionBarcodes) {
+        for (Barcode barcode : firebaseVisionBarcodes) {
+            communicator.qrRead(barcode.getRawValue());
+        }
+        processLatest();
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+        Log.w(TAG, "Barcode Reading Failure: ", e);
+    }
+}
